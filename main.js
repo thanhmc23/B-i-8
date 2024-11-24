86d3d74782b9f6e72d0bd2ae759c2c7ce1b71c79
thanhmc23 <thanhmc177@gmail.com>
2024-11-24 17:36:15 +0700

    ver3

--- main.js
+++ main.js
@@ -1,4 +1,19 @@
-fetch("https://pokeapi.co/api/v2/pokemon/ditto")
+fetch("https://pokeapi.co/api/v2/pokemon/charizard")
 .then(res=> res.json())
-.then(data=>console.log(data))
+.then(data=>{
+    console.log(data)
+    console.log(data.name)
+    console.log(data.weight)
+    console.log(data.height)
+    console.log(data.sprites.back_default)
+    for (let i = 0; i < data.types.length; i++) {
+        const type = data.types[i]; 
+        console.log(type.type.name);
+    }
+    for (let i = 0; i < data.abilities.length; i++) {
+        const type = data.abilities[i]; 
+        console.log(type.ability.name);
+    }
+    
+})
 .catch(err => console.log(err))
